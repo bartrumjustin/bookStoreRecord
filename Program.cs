0a050272fab2a43af2a354eb57bfa1555bdfa46f
bartrumjustin <bartrumjustin@gmail.com>
2026-02-22 21:32:26 -0500

    Finished ex 1 showing 3 examples of using a record and positional syntax

--- Program.cs
+++ Program.cs
@@ -2,9 +2,13 @@
 {
     internal class Program
     {
+        public record Bookstore(int ID, string Title, string Author, double Price);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Bookstore classic = new(001, "Moby Dick", "Abraham Shamis", 10.10);
+            Bookstore greek = new(002, "Illiad", "Homer", 12.00);
+            Bookstore comedy = new(003, "A Summer's night dream", "William Shakespear", 20.00);
+            Console.WriteLine(classic + "\n" + greek + "\n" + comedy);
         }
     }
 }
